0082a3778e3a98e496c48c2bfd9c6858aa4d01ba
msh <msh1562@naver.com>
2025-06-19 13:39:12 +0900

    fix json

--- lexis-web/next.config.ts
+++ lexis-web/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  experimental: {
+    appDir: true, // ? App Router È°¼ºÈ­!
+  },
 };
 
 export default nextConfig;
